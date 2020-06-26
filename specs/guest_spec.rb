9d1539f11eb9a4a217befc495dfdc94e4487c20f
Stephen <stephen_h369@live.co.uk>
2020-06-26 20:43:20 +0100

    get guest name passes

---
+++ specs/guest_spec.rb
@@ -0,0 +1,21 @@
+require('minitest/autorun')
+require('minitest/reporters')
+
+require_relative('../Guest')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
+
+class TestGuest < Minitest::Test
+
+    def setup()
+        @guest1 = Guest.new("Stephen", 100, "Highway To Hell", "Rock")
+        @guest2 = Guest.new("Erin", 1000, "Firework", "Pop")
+        @guest3 = Guest.new("Big Chungus", 9000, "Without Me", "Hip Hop")
+        @guest4 = Guest.new("Lil Doggo", 420, "Lose Yourself", "Hip Hop")
+    end
+
+    def test_get_guest_name()
+        assert_equal("Stephen", @guest1.name())
+    end
+
+end
