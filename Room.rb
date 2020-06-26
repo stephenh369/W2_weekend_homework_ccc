febfcc6bb6f29a9810a1cf21c735a2d46ce9f972
Stephen <stephen_h369@live.co.uk>
2020-06-26 22:10:17 +0100

    check in/check out passes

--- Room.rb
+++ Room.rb
@@ -6,13 +6,14 @@ class Room
         @room_name = room_name
         @room_guests = []
         @songs = []
+        @admission_fee = 10
     end
 
     def check_in_guest(guest)
-
+        @room_guests << guest
     end
 
     def check_out_guest(guest)
-
+        @room_guests.delete(guest)
     end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -11,7 +11,37 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
 class TestRoom < Minitest::Test
 
     def setup()
+        @room1 = Room.new("Rock Room")
+        @room2 = Room.new("Hip Hop Room")
+        @room3 = Room.new("Pop Room")
+        
+        @guest1 = Guest.new("Stephen", 100, "Highway To Hell", "Rock")
+        @guest2 = Guest.new("Erin", 1000, "Bootylicious", "Pop")
+        @guest3 = Guest.new("Big Chungus", 9000, "Without Me", "Hip Hop")
+        @guest4 = Guest.new("Lil Doggo", 420, "Lose Yourself", "Hip Hop")
+        @guest5 = Guest.new("Karen", 1, "Firework", "Pop")
+        
+        @song1 = Song.new("AC/DC", "Highway To Hell", "Rock")
+        @song2 = Song.new("Eminem", "Lose Yourself", "Hip Hop")
+        @song3 = Song.new("Katy Perry", "Firework", "Pop")
+        @song4 = Song.new("Eminem", "Without Me", "Hip Hop")
+        @song5 = Song.new("Destiny's Child", "Bootylicious", "Pop")
 
+        @songs = [@song1, @song2, @song3, @song4, @song5]
+        @song_collection = SongCollection.new(@songs)
+    end
+
+    def test_check_in_guest_free()
+        @room1.check_in_guest(@guest1)
+        assert_equal([@guest1], @room1.room_guests())
+    end
+
+    def test_check_out_guest()
+        @room1.check_in_guest(@guest1)
+        @room1.check_in_guest(@guest2)
+        @room1.check_in_guest(@guest3)
+        @room1.check_out_guest(@guest1)
+        assert_equal([@guest2, @guest3], @room1.room_guests())
     end
 
 end
